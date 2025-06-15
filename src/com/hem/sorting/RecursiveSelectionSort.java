c4bbd9d0ea419e439439c40e210a1943ebdc4bdd
hemadri-git <bathulahemadri@gmail.com>
2025-06-15 15:25:41 +0530

    recursion sorting

--- src/com/hem/sorting/RecursiveSelectionSort.java
+++ src/com/hem/sorting/RecursiveSelectionSort.java
@@ -1,26 +1,33 @@
-package com.hem.test;
+package com.hem.sorting;
+
 import java.util.Arrays;
-public class Test {
+
+public class RecursiveSelectionSort
+{
     public static void main(String[] args) {
+
         int[] arr = {1, 4, 32, 53, 2, 11};
         selection(arr, arr.length, 0, 0);
         System.out.println(Arrays.toString(arr));
     }
-    static void selection(int[] arr, int r, int c, int max) {
-        if (r == 0) {
+
+    private static void selection(int[] arr, int r, int c, int max) {
+
+        if (r == 0){
             return;
         }
-        if (c < r) {
-            if (arr[c] > arr[max]) {
+        if (c<r){
+            if (arr[c] > arr[max]){
                 selection(arr, r, c+1, c);
-            } else {
+            }else {
                 selection(arr, r, c+1, max);
             }
-        } else {
+        }
+        else {
             int temp = arr[max];
             arr[max] = arr[r-1];
             arr[r-1] = temp;
-            selection(arr, r-1, 0, 0);
+            selection(arr, r-1, 0,0);
         }
     }
 }
